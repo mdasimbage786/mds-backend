fc8e991462f3050a0bfc7f95c35796da3d279c41
Mohammedasim Bage <mdasimbage786@gmail.com>
2025-06-12 17:01:26 +0530

    Update CORS to Connect Frontend

--- src/main/java/com/pvncodes/MD/Config/WebConfig.java
+++ src/main/java/com/pvncodes/MD/Config/WebConfig.java
@@ -1,15 +1,21 @@
-package com.pvncodes.MD.config;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000") // Change this to match your React app's URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://astounding-snickerdoodle-449557.netlify.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
